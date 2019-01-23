a3cbfe8e478e96b7bf357f0694fba5ece063a928
Akihiro <ccleopa83@gmail.com>
2019-01-23 20:53:20 +0900

    23日の授業終わり！
    背景が動くところまで！

---
+++ Assets/03_Scripts/ScrollObject.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollObject : MonoBehaviour
+{
+	public float speed = 1.0f;
+	public float startPosition;
+	public float endPosition;
+
+	void Update ()
+	{
+		//毎フレームｘポジションを少しずつ移動させる
+		transform.Translate(-1 * speed * Time.deltaTime, 0, 0);
+
+		//スクロールが目標ポイントまで到達したかをチェック
+		if (transform.position.x <= endPosition) ScrollEnd();
+	}
+
+	void ScrollEnd()
+	{
+		//スクロールする距離分を戻す
+		transform.Translate(-1 * (endPosition - startPosition), 0, 0);
+
+		//同じゲームオブジェクトにアタッチされているコンポート年とにメッセージを送る
+		SendMessage("OnScrollEnd", SendMessageOptions.DontRequireReceiver);
+	}
+}
